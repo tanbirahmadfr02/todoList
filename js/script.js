86f579952e50a83e4f070b4834ebee66d654c2a0
tanbirahmadfr <018tanbirahmad@gmail.com>
2023-11-15 18:58:59 +0600

    odified

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
 let taskInput = document.getElementById("taskInput"),
   addTask = document.getElementById("addTask"),
   listContainer = document.getElementById("listContainer");
+let addSound = new Audio("addTask.mp3");
+let deleteTask = new Audio("deleteTask.mp3");
+let complete = new Audio("complete.mp3");
 
 addTask.addEventListener("click", function () {
   if (taskInput.value == "") {
-    alert("You must have to write something!")
+    alert("You must have to write something!");
   } else {
     let li = document.createElement("li");
     li.innerHTML = taskInput.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = " \u00d7 ";
-    li.appendChild(span)
+    li.appendChild(span);
+    addSound.play();
 
     taskInput.value = "";
-    saveData()
+    saveData();
   }
-})
+});
 
 listContainer.addEventListener("click", function (e) {
   if (e.target.tagName == "LI") {
     e.target.classList.toggle("checked");
-    saveData()
+    complete.play();
+    saveData();
   } else if (e.target.tagName == "SPAN") {
     e.target.parentElement.remove();
-    saveData()
+    saveData();
+    deleteTask.play();
   }
-})
+});
 
 function saveData() {
-  localStorage.setItem("data", listContainer.innerHTML)
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showTask() {
